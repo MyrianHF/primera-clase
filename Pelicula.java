bd6f2c37528edfea09b05f0149ca6a11083e9dfb
Myrian <myrianhf95@gmail.com>
2021-11-03 14:11:30 +0100

    Commit inicial

---
+++ Pelicula.java
@@ -0,0 +1,97 @@
+public class Pelicula {
+    
+    // Nombre del director
+    private String director;
+    // Número de actores
+    private int numeroActores;
+    // Película terminada
+    private boolean peliculaTerminada;
+
+    /**
+     * Crea película con director, número de actores y su estado.
+     */
+    public Pelicula(String nombreDirector, boolean peliculaFinalizada) {
+        director = nombreDirector;
+        numeroActores = 20;
+        peliculaTerminada = peliculaFinalizada;
+    }
+    
+    /**
+     * Devuelve el nombre del director
+     */
+    public String getNombreDirector() {
+        return director;
+    }   
+    
+    /**
+     * Devuelve el número de actores
+     */
+    public int getNumeroActores() {
+        return numeroActores;
+    }  
+
+    /**
+     * Devuelve el estado de la película
+     */
+    public boolean getPeliculaTerminada() {
+        return peliculaTerminada;
+    }  
+    
+    /**
+     * Establece nombre director del nuevo director
+     */
+    public void setNombreDirector(String nombreNuevoDirector) {
+        director = nombreNuevoDirector;
+    }   
+    
+    /**
+     * Establece el número de actores actuales
+    */
+    public void setNumeroActoresActual(int numeroActoresActual) {
+        numeroActores = numeroActoresActual;
+    }   
+    
+    /**
+     * Establece el estado de la película actual
+     */
+    public void setEstadoPelicula(boolean nuevoEstadoPelicula) {
+        peliculaTerminada = nuevoEstadoPelicula;
+    }   
+    
+    /**
+     * Añade el número de actores nuevos
+     */
+    public void modificarNumeroActores(int nuevosActoresIncorporados) {
+        numeroActores = numeroActores + nuevosActoresIncorporados;
+    }  
+
+    /**
+     * Cambia el estado de la película
+     */
+    public void cambioEstadoPelicula() {
+        if (peliculaTerminada == true) {
+            peliculaTerminada = false;
+        }
+        else {
+            peliculaTerminada = true;
+        }    
+    }
+    
+    /**
+     * Imprime detalles película
+     */
+    public void imprimirDetalles() {
+        System.out.println("Director: " + director);
+        System.out.println("Número total de actores actuales: " + numeroActores);
+        System.out.println("Película terminada: " + peliculaTerminada);
+    }
+    
+    /**
+     * Explica el estado de la película
+     */
+    public String estadoActualPelicula() {
+        String estadoActualPelicula;
+        estadoActualPelicula = director + numeroActores + peliculaTerminada;
+        return estadoActualPelicula;
+    }
+}
